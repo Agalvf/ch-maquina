a54d2283ba260de13b0b6e9c72bf60e964391b4e
Alejandro Galvis Florez <90481045+Agalvf@users.noreply.github.com>
2022-09-18 14:36:39 -0500

    Ajustes

--- src/App.vue
+++ src/App.vue
@@ -35,23 +35,26 @@ const cargar = () => {
         const lines = text.toString().split("\n");
         instrucciones.value = []
         variables.value = []
+        etiquetas.value = []
         lines.forEach(element => {
           element = element.trim()
           if (element.includes("nueva")) {
             let a = element.split(" ")
-            console.log(a)
             if (a[1] == "") {
               variables.value.push(a[2])
             }
-            else{
+            else {
               variables.value.push(a[1])
             }
           }
-          if(element.includes("etiqueta")){
+          if (element.includes("etiqueta")) {
             let a = element.split(" ")
             etiquetas.value.push(a[1])
           }
-          instrucciones.value.push(element)
+          if (element.includes("//") == false && element != "") {
+            console.log(element)
+            instrucciones.value.push(element)
+          }
         });
         for (let i = 1; i <= empiezaKernel.value; i++) {
           memoriaPrincipal.value[i] = "--** CH MAQUINA **--"
@@ -69,87 +72,89 @@ const cargar = () => {
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link" aria-current="page" href="#" @click="cargar">Home</a>
-          <a class="nav-link" href="#">Features</a>
-          <a class="nav-link" href="#">Pricing</a>
+  <div class="">
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link" aria-current="page" href="#" @click="cargar">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Pricing</a>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
-  <h1>CH maquina</h1>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <p>Memoria</p>
-        <input type="number" v-model="empiezaMemoria">
-        <p>Kernel</p>
-        <input type="number" v-model="empiezaKernel" :maxLength=max>
-        <div class="row py-5">
-          <div class="col">
+    </nav>
+    <h1>CH maquina</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <p>Memoria</p>
+          <input type="number" v-model="empiezaMemoria">
+          <p>Kernel</p>
+          <input type="number" v-model="empiezaKernel" :maxLength=max>
+          <div class="row py-5">
+            <div class="col">
+              <table class="table table-bordered table-striped mb-0 table-dark table-hover" id="tablaMemoria">
+                <thead>
+                  <tr>
+                    <th scope="col">Instrucciones</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(contenido, index) in instrucciones" :key="index">
+                    <td>{{ contenido }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="col">
+              <table class="table table-bordered table-striped mb-0 table-dark table-hover" id="tablaMemoria">
+                <thead>
+                  <tr>
+                    <th scope="col">Variables</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(contenido, index) in variables" :key="index">
+                    <td>{{ contenido }}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <table class="table table-bordered table-striped mb-0 table-dark table-hover my-2" id="tablaMemoria">
+                <thead>
+                  <tr>
+                    <th scope="col">Etiquetas</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(contenido, index) in etiquetas" :key="index">
+                    <td>{{ contenido }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <p>Hola</p>
+        </div>
+        <div class="col">
+          <div class="table-wrapper-scroll-y my-custom-scrollbar">
             <table class="table table-bordered table-striped mb-0 table-dark table-hover" id="tablaMemoria">
               <thead>
                 <tr>
-                  <th scope="col">Instrucciones</th>
+                  <th scope="col">Posición</th>
+                  <th scope="col">Valor</th>
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="(contenido, index) in instrucciones" :key="index">
+                <tr v-for="(contenido, index) in memoriaPrincipal" :key="index">
+                  <th scope="row">{{ index }}</th>
                   <td>{{ contenido }}</td>
                 </tr>
               </tbody>
             </table>
           </div>
-          <div class="col">
-            <table class="table table-bordered table-striped mb-0 table-dark table-hover" id="tablaMemoria">
-              <thead>
-                <tr>
-                  <th scope="col">Variables</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(contenido, index) in variables" :key="index">
-                  <td>{{ contenido }}</td>
-                </tr>
-              </tbody>
-            </table>
-            <table class="table table-bordered table-striped mb-0 table-dark table-hover my-2" id="tablaMemoria">
-              <thead>
-                <tr>
-                  <th scope="col">Variables</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(contenido, index) in etiquetas" :key="index">
-                  <td>{{ contenido }}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <p>Hola</p>
-      </div>
-      <div class="col">
-        <div class="table-wrapper-scroll-y my-custom-scrollbar">
-          <table class="table table-bordered table-striped mb-0 table-dark table-hover" id="tablaMemoria">
-            <thead>
-              <tr>
-                <th scope="col">Posición</th>
-                <th scope="col">Valor</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(contenido, index) in memoriaPrincipal" :key="index">
-                <th scope="row">{{ index }}</th>
-                <td>{{ contenido }}</td>
-              </tr>
-            </tbody>
-          </table>
         </div>
       </div>
     </div>
